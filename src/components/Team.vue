8eaa34d1fbeac9d0a660feb5a4c50abc3d7ee665
Piccolo <kabiryusufbashir@gmail.com>
2022-03-22 20:42:52 +0100

    Create Team component

---
+++ src/components/Team.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="bg-white">
+        <div class="px-24 py-8">
+            <div class="text-center text-3xl my-8 border-b">
+                Work Force
+            </div>
+
+            <div class="grid grid-cols-3 gap-4">
+                <div class="slider-info col-lg-4 col-md-6 text-center">
+                    <div class="py-4 border rounded-xl">
+                        <a href="/about#team"><img src="../assets/images/ceo-2.jpg" alt="Ahmed Yusuf" class="mx-auto py-4"><hr>
+                        <h5 class="py-4">Mr. Ahmed Yusuf</h5><hr>
+                        <p style="color:#e37400;" class="py-4">Founder and C.E.O</p>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="slider-info col-lg-4 col-md-6 text-center">
+                    <div class="py-4 border rounded-xl">
+                        <a href="/about#team"><img class="mx-auto py-4" src="../assets/images/dir-2.jpg" alt="Mahmud Sani Usman"><hr>
+                            <h5 class="py-4">Mr. Mahmud Sani Usman</h5><hr>
+                            <p style="color:#e37400;" class="py-4">Marketing and International affairs</p>
+                        </a>
+                    </div>
+                </div>
+
+                <div class="slider-info col-lg-4 col-md-6 text-center">
+                    <div class="py-4 border rounded-xl">
+                        <a href="/about#team"><img src="../assets/images/cto-2.jpg" alt="Mr. Ismail Abdulmalik" class="mx-auto py-4 "><hr>
+                            <h5 class="py-4">Mr. Ismail Abdulmalik</h5><hr>
+                            <p style="color:#e37400;" class="py-4">C.T.O</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+   export default {
+       name: 'Team'
+   }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,22 @@
   <Header />
   <Banner />
   <Commodity />
+  <Team />
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
 import Banner from '@/components/Banner.vue'
 import Commodity from '@/components/Commodity.vue'
+import Team from '@/components/Team.vue'
 
 export default {
   name: "Home",
   components: {
     Header,
     Banner,
-    Commodity
-},
+    Commodity,
+    Team
+  },
 }
 </script>
